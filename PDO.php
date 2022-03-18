559a7140f583d1c9f010ed53be42bffaf46c87c8
ksenijacer <ksenijacerovic7@gmail.com>
2022-03-18 21:44:54 +0100

    added function

--- PDO.php
+++ PDO.php
@@ -23,3 +23,9 @@ function getDataFromServer($sql, $connection, $isFetchAll)
 
   return $statement->fetch();
 };
+
+function setDataToServer($sql, $connection)
+{
+  $statement = $connection->prepare($sql);
+  $statement->execute();
+};
--- posts.php
+++ posts.php
@@ -6,24 +6,21 @@ $sql = "SELECT * FROM posts ORDER BY created_at DESC";
 $posts = getDataFromServer($sql, $connection, true);
 ?>
 
-<?php include('header.php'); ?>
-
 <main role="main" class="container">
     <div class="row">
         <div class="col-sm-8 blog-main">
 
             <?php foreach ($posts as $post) { ?>
-            <div class="blog-post">
+                <div class="blog-post">
 
-                <a href="single-post.php?id=<?php echo $post['id'] ?>" class="">
-                    <h2 class="blog-post-title"><?php echo $post['title'] ?> </h2>
-                </a>
+                    <a href="single-post.php?id=<?php echo $post['id'] ?>" class="">
+                        <h2 class="blog-post-title"><?php echo $post['title'] ?> </h2>
+                    </a>
 
-                <p class="blog-post-meta"><?php date_format(date_create($post['created_at']), 'd-F-Y') ?> by <a
-                        href="#"><?php echo $post['author'] ?></a></p>
+                    <p class="blog-post-meta"><?php date_format(date_create($post['created_at']), 'd-F-Y') ?> by <a href="#"><?php echo $post['author'] ?></a></p>
 
-                <p><?php echo $post['body'] ?></p>
-            </div>
+                    <p><?php echo $post['body'] ?></p>
+                </div>
             <?php } ?>
 
             <nav class="blog-pagination">
